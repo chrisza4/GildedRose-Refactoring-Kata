311423a1b1e1ed678b0ab70831eba72792714668
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2020-04-27 20:27:12 +0700

    Name properly

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -8,81 +8,76 @@ defmodule GildedRose do
   end
 
   def update_item(item) do
+    # Main
     item
-    |> update_item_step_1()
+    |> update_today_quality()
     |> decrease_days_remaining()
-    |> update_item_step_2()
+    |> update_quality_if_expired()
   end
 
-  defp update_item_step_1(%Item{name: "Aged Brie"} = item) do
-    cond do
-      item.quality >= 50 ->
-        item
-
-      true ->
-        %{item | quality: item.quality + 1}
-    end
+  defp update_today_quality(%Item{name: "Aged Brie"} = item) do
+    increase_quality(item)
   end
 
-  defp update_item_step_1(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
+  defp update_today_quality(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
     cond do
-      item.quality >= 50 ->
-        item
-
       item.days_remaining < 6 ->
         item
-        |> inc_quality_promotionally()
-        |> inc_quality_promotionally()
-        |> inc_quality_promotionally()
+        |> increase_quality()
+        |> increase_quality()
+        |> increase_quality()
 
       item.days_remaining < 11 ->
         item
-        |> inc_quality_promotionally()
-        |> inc_quality_promotionally()
+        |> increase_quality()
+        |> increase_quality()
 
       true ->
-        inc_quality_promotionally(item)
+        increase_quality(item)
     end
   end
 
-  defp update_item_step_1(%Item{name: "Sulfuras, Hand of Ragnaros"} = item) do
+  defp update_today_quality(%Item{name: "Sulfuras, Hand of Ragnaros"} = item) do
     item
   end
 
-  defp update_item_step_1(item) do
-    %{item | quality: max(0, item.quality - 1)}
+  defp update_today_quality(item) do
+    decrease_quality(item)
   end
 
   defp decrease_days_remaining(%Item{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
   defp decrease_days_remaining(item), do: %{item | days_remaining: item.days_remaining - 1}
 
   # Step 2
-  defp update_item_step_2(%Item{days_remaining: days_remaining} = item)
+  defp update_quality_if_expired(%Item{days_remaining: days_remaining} = item)
        when days_remaining >= 0 do
+    # Not expired yet
     item
   end
 
-  defp update_item_step_2(%Item{name: "Aged Brie"} = item) do
-    inc_quality_promotionally(item)
+  defp update_quality_if_expired(%Item{name: "Aged Brie"} = item) do
+    increase_quality(item)
   end
 
-  defp update_item_step_2(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
+  defp update_quality_if_expired(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
     %{item | quality: 0}
   end
 
-  defp update_item_step_2(%Item{name: "Sulfuras, Hand of Ragnaros"} = item) do
+  defp update_quality_if_expired(%Item{name: "Sulfuras, Hand of Ragnaros"} = item) do
     item
   end
 
-  defp update_item_step_2(item) do
-    %{item | quality: max(0, item.quality - 1)}
+  defp update_quality_if_expired(item) do
+    decrease_quality(item)
   end
 
-  defp inc_quality_promotionally(%Item{} = item) do
+  defp increase_quality(%Item{} = item) do
     if item.quality >= 50 do
       item
     else
       %{item | quality: item.quality + 1}
     end
   end
+
+  defp decrease_quality(item), do: %{item | quality: max(0, item.quality - 1)}
 end
